1d241cdce3575f66c5c19c8c7801f2fc0986839a
Pablo <Pablo@DESKTOP-7F9AMFF>
2023-06-14 09:47:01 +0200

    Corregida la fórmula para que no sean enteros

--- src/Ejercicios1/Ej8.java
+++ src/Ejercicios1/Ej8.java
@@ -9,7 +9,7 @@ public class Ej8 {
 		System.out.println("Introduce el radio");
 		float radio=teclado.nextFloat();
 		
-		double volumen=4/3*Math.PI*Math.pow(radio,3);
+		double volumen=4/3.0*Math.PI*Math.pow(radio,3);
 			
 		System.out.printf("La esfera con radio %.0f tiene un volumen de %.2f",radio,volumen);
 		
